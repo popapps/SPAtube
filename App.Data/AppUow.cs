2d8af138bd17ebc892ab4da946200a2a23c6b914
popapps <info@popapps.it>
2012-10-18 11:44:27 +0200

    .

--- App.Data/AppUow.cs
+++ App.Data/AppUow.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
 using App.Data.Contracts;
 using App.Data.Helpers;
-using App.Model;
 
 namespace App.Data
 {
